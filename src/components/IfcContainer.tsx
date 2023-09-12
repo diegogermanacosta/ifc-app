75f1a54745ccff8b61d78eb9f53f6fed366513f3
yofalox <43282640+yofalox@users.noreply.github.com>
2023-09-12 09:12:12 -0300

    Initial commit

---
+++ src/components/IfcContainer.tsx
@@ -0,0 +1,93 @@
+import { useRef, useState, useLayoutEffect, useEffect, KeyboardEvent } from 'react'
+import { IfcViewerAPI } from 'web-ifc-viewer'
+import { Color } from 'three'
+
+const IfcContainer = () => {
+  const ifcContainer = useRef<HTMLDivElement>(null)
+  const [initialViewer, setInitialViewer] = useState<IfcViewerAPI | null>(null)
+  const [viewer, setViewer] = useState<IfcViewerAPI | null>(null)
+
+  const loadIfc = async (container: HTMLDivElement) => {
+    const ifcViewer = new IfcViewerAPI({ container, backgroundColor: new Color(0xffffff) })
+    ifcViewer.axes.setAxes()
+    ifcViewer.grid.setGrid()
+    await ifcViewer.IFC.loader.ifcManager.applyWebIfcConfig({
+      COORDINATE_TO_ORIGIN: true,
+      USE_FAST_BOOLS: false
+    })
+    return ifcViewer
+  }
+
+  const loadModel = async (viewer: IfcViewerAPI, url: string) => {
+    await viewer.IFC.setWasmPath('./')
+    const model = await viewer.IFC.loadIfcUrl(url)
+    viewer.shadowDropper.renderShadow(model.modelID)
+    viewer.clipper.active = true
+    setInitialViewer(null)
+  }
+
+  const ifcCleanup = async (ifcViewer: IfcViewerAPI) => {
+    await ifcViewer.dispose()
+  }
+
+  const handleOnDoubleClick = () => {
+    if (!viewer) return
+    viewer.IFC.selector.pickIfcItem(true)
+  }
+
+  const handleMouseMove = () => {
+    if (!viewer) return
+    viewer.IFC.selector.prePickIfcItem()
+  }
+
+  const handleKeyDown = (event: KeyboardEvent<HTMLDivElement>) => {
+    if (!viewer) return
+    if(event.code === 'KeyP') {
+      viewer.clipper.createPlane()
+    }
+    if(event.code === 'KeyO') {
+      viewer.clipper.deletePlane()
+    }
+  }
+
+  useLayoutEffect(() => {
+    let ifcViewer: IfcViewerAPI
+
+    const initialize = async (ifcContainer: HTMLDivElement) => {
+      ifcViewer = await loadIfc(ifcContainer)
+      setInitialViewer(ifcViewer)
+      setViewer(ifcViewer)
+    }
+
+    if (ifcContainer.current) {
+      initialize(ifcContainer.current)
+    }
+
+    return () => {
+      if (ifcViewer) {
+        ifcCleanup(ifcViewer)
+      }
+    }
+  }, [])
+
+  useEffect(() => {
+    if (initialViewer) {
+      let params = new URL(document.location.href).searchParams;
+      const urlFile =  params.get("url");
+      if(!urlFile) return;
+      loadModel(initialViewer, urlFile)
+    }
+  }, [initialViewer])
+
+  return (
+    <div className={'ifcContainer'}
+      ref={ifcContainer}
+      tabIndex={0}
+      onDoubleClick={handleOnDoubleClick}
+      onMouseMove={handleMouseMove}
+      onKeyDown={event => handleKeyDown(event)}
+    />
+  )
+}
+
+export default IfcContainer
---
+++ src/components/SpatialTree.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const SpatialTree = (ifcProyext : object) => {
+  return (
+    <div>SpatialTree</div>
+  )
+}
+
+export default SpatialTree
+
+const CreateNode = (parent : object, text : string, children : object) => {
+  return (
+    <div>CreateNode</div>
+  )
+}
+
+const CreateBranchNode = () => {
+  return (
+    <div>CreateBranchNode</div>
+  )
+}
+const CreateLeafNode = () => {
+  return (
+    <div>CreateLeafNode</div>
+  )
+}
